7ad9742655a0c880d866721ce90bdb5ef8e37ed2
Roni_Pick <ronipick3@gmail.com>
2021-11-10 17:12:05 +0200

    elev

---
+++ venv/Elevator.py
@@ -0,0 +1,13 @@
+class Elevator:
+    def __init__(self, id, speed, minFloor, maxFloor, openTime, closeTime, timeToStart, timeToStop):
+        self.id=id
+        self.speed=speed
+        self.minFloor=minFloor
+        self.maxFloor=maxFloor
+        self.openTime=openTime
+        self.closeTime=closeTime
+        self.timeToStart=timeToStart
+        self.timeToStop=timeToStop
+
+
+
