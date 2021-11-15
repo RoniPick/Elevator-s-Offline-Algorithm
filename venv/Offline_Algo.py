44a74e88fddafa7f0ef3b3f9f65a6c7029349d4b
RoniPick <92802335+RoniPick@users.noreply.github.com>
2021-11-15 11:03:08 +0200

    Update Offline_Algo.py
    update

--- venv/Offline_Algo.py
+++ venv/Offline_Algo.py
@@ -6,13 +6,13 @@ import Calls
 
 
 class Offline_Algo:
-
-    def main_function(Building_File, Calls_File, output_File): # the main function of the program. we receive the files and we will need to return a new file
-    # the load function will be in the building and calls class
-    # the creation of the return file will be in a function inside the calls class
+    ## the main function of the program. we receive the files and we will need to return a new file
+    ## the load function will be in the building and calls class
+    ## the creation of the return file will be in a function inside the calls class
     ## --> after we finished with the creation we will send the list of calls / a single call
     ##  and the elevator's list to the allocate_call function
     ## at the end of the function we will write a new scv file and we will return it.
+    def main_function(Building_File, Calls_File, output_File):
     building =
     calls =
 
@@ -35,21 +35,25 @@ class Offline_Algo:
                 calls.append(c)
         return calls
 
-
-    def allocate_call(calls, []) # this function will get a list of calls /a single call and allocate the call at an elevator
+    ## this function will get a list of calls /a single call and allocate the call at an elevator
     ## we will run all over the new list we created to the calls - as long as the list isn't empty.
     ## if we have only one elevator - send the call to this elevator for all the calls.
     ## if we have more than one elevator - for every call we will run all over the elevators that in the building
     ## we will use the best_time and calculate_time function
     ## after we will allocate the call to the elevator we will change the location number to the relevant elevator.
+    def allocate_call(calls, []):
 
-    def best_time():  # this call will tell us which elevator is the best for the specific call
+
+    # this call will tell us which elevator is the best for the specific call
     ## we will get a call from the call's list and the list of the elevators that in the building.
     ## then, we will send a specific elevator and the call to the calculate_time function
+    def best_time():
 
-    def calculate_time(calls,elevator): # this function will calculate the total time for a call in specific elevator
+    # this function will calculate the total time for a call in specific elevator
     ## that means: for every elevator we will check the time for the call to be completed and then we will save it in the
     ## totalTime parameter we created inside the call.
+    def calculate_time(calls,elevator):
+
 
     if __name__ == '__main__':
         main_function()
